39cd53c6c95600f7cee65dd84578a91471b5320f
prathiuviswak <prathiuviswakdev@gmail.com>
2024-03-27 09:50:59 +0530

    deployed

--- Booth_Backend/api.js
+++ Booth_Backend/api.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 
 app.use(cors(
     {
-            origin: ["https://deploy-boothlist-frontend.vercel.app/"],
+            origin: ["https://deploy-boothlist-frontend.vercel.app"],
             methods: ["POST", "GET"],
             credentials: true
           }
