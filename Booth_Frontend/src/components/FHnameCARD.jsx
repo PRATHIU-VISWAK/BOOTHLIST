2dfca228b202b8cdce75a4b892ecf8333d117258
prathiuviswak <prathiuviswakdev@gmail.com>
2024-03-27 13:25:33 +0530

    deployed

--- Booth_Frontend/src/components/FHnameCARD.jsx
+++ Booth_Frontend/src/components/FHnameCARD.jsx
@@ -2,6 +2,8 @@ export const FHnameCARD = ({ name }) => {
   return (
     <div className="bg-gray-900 text-white p-4 rounded-lg mb-4 text-left">
       <h2 className="text-xl font-semibold">BOOTH : {name?.Booth}</h2>
+      <h2 className="text-xl font-semibold">NAME : {name?.SN}</h2>
+      <h2 className="text-xl font-semibold">NAME : {name?.PN}</h2>
       <h2 className="text-xl font-semibold">NAME : {name?.Name}</h2>
       <h2 className="text-xl font-semibold">AGE : {name?.Age}</h2>
       <h2 className="text-xl font-semibold">Voter ID : {name?.VoterID}</h2>
