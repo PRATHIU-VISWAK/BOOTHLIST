d6d9fca033265ea307f3830ba95ded97c7a9a59d
prathiuviswak <prathiuviswakdev@gmail.com>
2024-03-28 10:07:01 +0530

    done some css

--- Booth_Frontend/src/pages/FetchID.jsx
+++ Booth_Frontend/src/pages/FetchID.jsx
@@ -2,8 +2,9 @@ import Axios from "axios";
 import { useState, useContext } from "react";
 import { AppContext } from "../App";
 import { useQuery } from "@tanstack/react-query";
+import "../App.css";
 import { DisplayCARD } from "../components/DisplayCARD";
-import "../App.css"
+
 export const FetchID = () => {
   const { INPUT, setINPUT } = useContext(AppContext);
   const { data, isLoading, refetch } = useQuery({
@@ -11,21 +12,22 @@ export const FetchID = () => {
     queryFn: async () => {
       try {
         const response = await Axios.post(
-          `${import.meta.env.VITE_API_URL}/booths/ID?name=${INPUT}`
+          `${import.meta.env.VITE_API_URL}/booths/ID?id=${INPUT}`
         );
-        // console.log(response.data);
+        //console.log(response.data);
         return response.data;
       } catch (error) {
-        // console.error("Error:", error);
+        console.error("Error:", error);
         throw error;
       }
     },
   });
+
   return (
     <div className="mt-4">
       <input
         type="text"
-        placeholder="Enter Voter Name"
+        placeholder="Enter Voter ID"
         className="border border-gray-300 rounded-lg px-4 py-2 mr-2"
         onChange={(event) => {
           setINPUT(event.target.value);
@@ -37,13 +39,25 @@ export const FetchID = () => {
       >
         GET Data
       </button>
+      {/* <div className="resultbg mt-4 bg-gray-900 text-white p-4 rounded-lg mb-4">
+        {isLoading ? (
+          <h2 className="result mt-4 text-white p-4 rounded-lg mb-4" >Loading...</h2>
+        ) : (
+          <>
+        <div>
+          <DisplayCARD name={data}/>
+        </div>
+        </>
+        )}
+      </div> */}
+
       <div className="resultbg mt-4 bg-gray-900 text-white p-4 rounded-lg mb-4">
         {isLoading ? (
           <h2>Loading...</h2>
         ) : (
           <div>
-              <DisplayCARD name={data}/>
-          </div>
+          <DisplayCARD name={data}/>
+        </div>
         )}
       </div>
     </div>
