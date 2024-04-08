4d0c21a1356ccab983e3538a89800f06dde8d99a
prathiuviswak <prathiuviswakdev@gmail.com>
2024-04-08 18:07:36 +0530

    done some css 2

--- Booth_Frontend/src/pages/FetchFHNAME.jsx
+++ Booth_Frontend/src/pages/FetchFHNAME.jsx
@@ -29,7 +29,7 @@ export const FetchFHNAME = () => {
       <input
         type="text"
         placeholder="Relative NAME"
-        className="border border-gray-300 rounded-lg px-20 py-2 mr-2"
+        className="border border-gray-300 rounded-lg px-4 py-2 mr-2"
         onChange={(event) => {
           setINPUT(event.target.value);
         }}
