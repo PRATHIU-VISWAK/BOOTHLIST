1608e49ae75b7f7f7f77a7c073bbfbf022371680
prathiuviswak <prathiuviswakdev@gmail.com>
2024-04-18 03:21:18 +0530

    some component done

--- Booth_Backend/api/index.js
+++ Booth_Backend/api/index.js
@@ -16,6 +16,15 @@ app.use(cors(
             credentials: true
           }
 ));
+app.use((req, res, next) => {
+    res.set({
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "*",
+        "Access-Control-Allow-Headers": "'Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'",
+    });
+
+    next();
+});
 app.use(express.json());
 
 // LIST = []
